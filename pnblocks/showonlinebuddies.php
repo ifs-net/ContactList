ba92b0ba355639583fe0723005e6140e455da329
quan <quan@e48c9bf7-d763-4473-bb20-e4d3470cb590>
2008-04-10 20:41:20 +0000

    new block: show my buddies that are online

---
+++ pnblocks/showonlinebuddies.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * initialise block
+ * 
+ */
+function ContactList_ShowOnlineBuddiesblock_init()
+{
+    pnSecAddSchema('ContactList:ShowOnlineBuddiesblock:', 'Block title::');
+}
+
+/**
+ * get information on block
+ * 
+ * @return       array       The block information
+ */
+function ContactList_ShowOnlineBuddiesblock_info()
+{
+    return array('module'         => 'ContactList',
+                 'text_type'      => 'ShowOnlineBuddies',
+                 'text_type_long' => 'Show user\'s buddies that are online',
+                 'allow_multiple' => true,
+                 'form_content'   => false,
+                 'form_refresh'   => false,
+                 'show_preview'   => false,
+                 'admin_tableless' => true);
+}
+
+/**
+ * display block
+ * 
+ * @param        array       $blockinfo     a blockinfo structure
+ * @return       output      the rendered bock
+ */
+function ContactList_ShowOnlineBuddiesblock_display($blockinfo)
+{
+    if (!pnUserLoggedIn() || !pnModAvailable('ContactList') || !SecurityUtil::checkPermission('ContactList:ShowOnlineBuddiesblock:', "$blockinfo[title]::", ACCESS_READ)) return false;
+
+    $render = pnRender::getInstance('ContactList', false);
+    $uid = pnUserGetVar('uid');
+	$buddies = pnModAPIFunc('ContactList','user','getall',
+									array(	'uid'		=> $uid,
+											'state'		=> 1,
+											'sort'		=> 'uname') );
+	$c=0;
+	if (!(count($buddies)>0)) return false;	// if there are no buddies return no content
+	else {
+	  	foreach ($buddies as $buddy) {
+		    if ($buddy['online']) {
+			  	$buddies_online[]=$buddy;
+			  	$c++;
+		    }
+		}
+	    $render->assign('buddies_online',				$buddies_online);
+	    $render->assign('buddies_online_counter',		$c);
+	    $blockinfo['content'] = $render->fetch('contactlist_block_showonlinebuddies.htm');
+	    return themesideblock($blockinfo);
+	}
+}
+
+/**
+ * modify block settings
+ * 
+ * @param        array       $blockinfo     a blockinfo structure
+ * @return       output      the bock form
+ */
+function ContactList_ShowOnlineBuddiesblock_modify($blockinfo)
+{
+    return "";
+}
+
+
+/**
+ * update block settings
+ * 
+ * @param        array       $blockinfo     a blockinfo structure
+ * @return       $blockinfo  the modified blockinfo structure
+ */
+function ContactList_ShowOnlineBuddiesblock_update($blockinfo)
+{
+    $blockinfo['content'] = pnBlockVarsToContent($vars);
+    return $blockinfo;
+}
+?>
---
+++ pnlang/eng/showonlinebuddies.php
@@ -0,0 +1,4 @@
+<?php
+define('_CONTACTLISTMYONLINEBUDDIES',			'Buddies online');
+define('_CONTACTLISTNOBUDDYONLINE',				'No buddy online');
+?>
