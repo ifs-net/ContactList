464a5123bc725576d08f383d1ac506f79fe416ae
herr_vorragend <herr_vorragend@e48c9bf7-d763-4473-bb20-e4d3470cb590>
2008-04-29 13:59:45 +0000

    svn-properties

--- includes/common.php
+++ includes/common.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package      ContactList
- * @version      $Id: common.php 134 2008-04-20 15:48:13Z quan $
+ * @version      $Id$
  * @author       Florian Schießl, Carsten Volmer
  * @link         http://www.ifs-net.de, http://www.carsten-volmer.de
  * @copyright    Copyright (C) 2008
