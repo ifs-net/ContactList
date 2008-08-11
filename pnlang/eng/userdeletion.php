75a48e7a60efb1c1b3e33422dc871aac5ff73d2f
quan <quan@e48c9bf7-d763-4473-bb20-e4d3470cb590>
2008-08-11 13:14:50 +0000

    Added UserDeletion support

---
+++ pnlang/eng/userdeletion.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package      ContactList
+ * @version      $Id: version.php 95 2008-04-12 19:06:51Z herr_vorragend $ 
+ * @author       Florian Schießl, Carsten Volmer
+ * @link         http://www.ifs-net.de, http://www.carsten-volmer.de
+ * @copyright    Copyright (C) 2008
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+define('_CONTACTLISTMODULETITLE', 					'Buddy and ignore list');
+define('_CONTACTLISTBUDDYLISTENTRIESAND',			'buddy and');
+define('_CONTACTLISTIGNORELISTENTRIESDELETEDFOR',	'ignore list entries deleted for user');
+?>
