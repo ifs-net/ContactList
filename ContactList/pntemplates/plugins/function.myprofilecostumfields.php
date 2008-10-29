43b7f6a391a62e639e9718b3223fc0ee3f67ec5e
quan <quan@e48c9bf7-d763-4473-bb20-e4d3470cb590>
2008-10-29 19:02:30 +0000

    plugin for myprofile customfields integration

---
+++ ContactList/pntemplates/plugins/function.myprofilecostumfields.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @package      ContactList
+ * @version      $Id: $
+ * @author       Florian Schießl, Carsten Volmer
+ * @link         http://www.ifs-net.de, http://www.carsten-volmer.de
+ * @copyright    Copyright (C) 2008
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+function smartfy_function_myprofilecustomfields($params, &$smarty) 
+{
+  echo "test";
+	return "hello world";
+}
+die("..");
